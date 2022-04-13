a22d0a8154a6cf24cb3ef63b38f5c633d7ac4071
Myles Spillers <43889349+MSpillers@users.noreply.github.com>
2022-04-12 23:13:34 -0500

    Added driver fixture

--- test_web_bmi.py
+++ test_web_bmi.py
@@ -3,17 +3,34 @@ from app import create_app
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
+@pytest.fixture()
+def driver():
+    service = FirefoxService(executable_path=GeckoDriverManager().install())
+    opts = Options()
+    opts.add_argument("--headless")
+    driver = webdriver.Firefox(options=opts,service=service)
+    return driver
 
-def test_firefox_session():
-   service = FirefoxService(executable_path=GeckoDriverManager().install())
-   opts = Options()
-   opts.add_argument("--headless")
-   driver = webdriver.Firefox(options=opts)
-   #driver.get("https://www.google.com")
-   #print(driver.title)
+def test_application_status(driver):
+   driver.get("https://bmi-web-test-qa.herokuapp.com/")
    driver.quit()
+
+#def test_form_submission(driver):
+#   driver.get("https://bmi-web-test-qa.herokuapp.com/")
+    #wait= WebDriverWait(driver,20)
+    #driver.find_element(By.ID,"height").send_keys("511" + Keys.ENTER)
+    #driver.find_elememt(By.ID,"weight").send_keys("200" + Keys.ENTER)
+    #driver.find_elememt(By.ID,"submit").click()
+    
+    #assert driver.title == "Results"
+#    driver.quit()
+
 
 
 @pytest.fixture()
@@ -37,12 +54,12 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
-def test_home_page(client):
+#def test_home_page(client):
 
-    #flask_app = create_app()
+#    #flask_app = create_app()
 
-    #with flask_app.test_client() as test_client:
-    response = client.get('/')
-    assert response.status_code == 200
+#    #with flask_app.test_client() as test_client:
+#    response = client.get('/')
+#   assert response.status_code == 200
 
 
